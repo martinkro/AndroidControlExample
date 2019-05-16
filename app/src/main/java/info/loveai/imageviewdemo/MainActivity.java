c69ac31d842e99fbe2e6ff69a1eea668a4f9e5f4
Jun Liu <liujun.sun@bytedance.com>
2019-05-16 11:59:21 +0800

    second activity

--- app/src/main/java/info/loveai/imageviewdemo/MainActivity.java
+++ app/src/main/java/info/loveai/imageviewdemo/MainActivity.java
@@ -1,13 +1,28 @@
 package info.loveai.imageviewdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_start_second).setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        int id = v.getId();
+        switch (id){
+            case R.id.btn_start_second:
+                startActivity(new Intent(this,SecondActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
